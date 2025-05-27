ee4c6491763faad0e60a6de06288e16bb2e2de58
Mohamed Shire <mohamed.shire@helsing.ai>
2025-05-27 17:27:00 +0100

    Intial VM impl to interpret chunks, included NEGATE,ADD,SUBTRACT,MULTIPLY,DIVIDE OP codes

--- src/value.c
+++ src/value.c
@@ -1,35 +1,29 @@
-#include <stdio.h>
 #include "value.h"
 #include "memory.h"
+#include <stdio.h>
 
-
-
-
-void init_value_array(ValueArray* value_array){
+void init_value_array(ValueArray *value_array) {
   value_array->count = 0;
   value_array->capacity = 0;
-  value_array->values= NULL;
+  value_array->values = NULL;
 }
 
-void write_value_array(ValueArray* value_array, Value value){
-  
+void write_value_array(ValueArray *value_array, Value value) {
   // When array out of space, reallocate
   if (value_array->capacity < value_array->count + 1) {
     int old_capacity = value_array->capacity;
     value_array->capacity = GROW_CAPACITY(old_capacity);
-    value_array->values =
-        GROW_ARRAY(Value, value_array->values, old_capacity, value_array->capacity);
+    value_array->values = GROW_ARRAY(Value, value_array->values, old_capacity,
+                                     value_array->capacity);
   }
 
   value_array->values[value_array->count] = value;
   value_array->count++;
 }
 
-void free_value_array(ValueArray* value_array){
+void free_value_array(ValueArray *value_array) {
   FREE_ARRAY(Value, value_array->values, value_array->capacity);
   init_value_array(value_array);
 }
 
-void print_value(Value value){
-  printf("%g'", value);
-}
+void print_value(Value value) { printf("%g", value); }
