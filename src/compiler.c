651da31759a02fef380996abfae1e4007de4d6fd
Mohamed Shire <mohamed.shire@helsing.ai>
2025-05-31 20:24:13 +0100

    WIP: scanner impl - parsing single character tokens, 2 character tokens + literals

--- src/compiler.c
+++ src/compiler.c
@@ -5,6 +5,7 @@
 #include "compiler.h"
 #include "scanner.h"
 
+// NOTE: C tosses enum names in the top-level namespace
 void compile(const char* source) {
   init_scanner(source);
 }
--- src/scanner.c
+++ src/scanner.c
@@ -1,17 +1,173 @@
 #include "scanner.h"
+#include <stdbool.h>
+#include <string.h>
 
 typedef struct {
-  const char* start;
-  const char* current;
+  const char *start;
+  const char *current;
   int line;
 } Scanner;
 
 Scanner scanner;
 
-void init_scanner(const char *source){
+void init_scanner(const char *source) {
   scanner.start = source;
   scanner.current = source;
   scanner.line = 1;
 }
+static bool is_at_end() { return *scanner.current == '\0'; }
 
+static Token make_token(TokenType type) {
+  Token token;
 
+  token.type = type;
+  token.start = scanner.start;
+  token.length = (int)(scanner.current - token.start);
+  token.line = scanner.line;
+
+  return token;
+}
+
+static Token error_token(const char *message) {
+  Token token;
+
+  token.type = TOKEN_EOF;
+  token.start = message;
+  token.length = (int)strlen(message);
+  token.line = scanner.line;
+
+  return token;
+}
+
+static char advance() {
+  scanner.current++;
+  return scanner.current[-1];
+}
+
+static bool match(char expected) {
+  if (is_at_end())
+    return false;
+
+  if (!(*scanner.current == expected))
+    return false;
+
+  scanner.current++;
+  return true;
+}
+
+static char peek() { return *scanner.current; }
+
+static char peek_next() {
+  if (is_at_end())
+    return '\0';
+  return scanner.current[1];
+}
+
+static void skip_whitespaces() {
+  for (;;) {
+    char c = peek();
+    switch (c) {
+    case ' ':
+    case '\t':
+    case '\r':
+      advance();
+      break;
+    case '\n':
+      scanner.line++;
+      advance();
+      break;
+    case '/':
+      if (peek_next() == '/') {
+        while (peek() != '\n' && !is_at_end())
+          advance();
+      } else {
+        return;
+      }
+      break;
+    default:
+      return;
+    }
+  }
+}
+
+static Token string() {
+  // Continue to advance the scanner cursor until we find the closing quote
+  while (peek() != '"' && !is_at_end()) {
+    if (peek() == '\n')
+      scanner.line++;
+  };
+
+  if (is_at_end())
+    return error_token("Unterminated string.");
+
+  // Consume closing quote
+  advance();
+
+  return make_token(TOKEN_STRING);
+}
+
+static bool is_digit(char c) { return c >= '0' && c <= '9'; }
+
+static Token number() {
+  while (is_digit(peek()))
+    advance();
+
+  // Handle decimal numbers
+  if (match('.')) {
+    // Consume the '.'
+    advance();
+    while (is_digit(peek()))
+      advance();
+  }
+
+  return make_token(TOKEN_NUMBER);
+}
+
+Token scan_token() {
+  skip_whitespaces();
+  scanner.start = scanner.current;
+
+  char c = advance();
+
+  if (is_digit(c))
+    return number();
+
+  switch (c) {
+  case '(':
+    return make_token(TOKEN_LEFT_PAREN);
+  case ')':
+    return make_token(TOKEN_RIGHT_PAREN);
+  case '{':
+    return make_token(TOKEN_LEFT_BRACE);
+  case '}':
+    return make_token(TOKEN_RIGHT_BRACE);
+  case ';':
+    return make_token(TOKEN_SEMICOLON);
+  case ',':
+    return make_token(TOKEN_COMMA);
+  case '.':
+    return make_token(TOKEN_DOT);
+  case '-':
+    return make_token(TOKEN_MINUS);
+  case '+':
+    return make_token(TOKEN_PLUS);
+  case '/':
+    return make_token(TOKEN_SLASH);
+  case '*':
+    return make_token(TOKEN_STAR);
+  case '!':
+    return make_token(match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
+  case '=':
+    return make_token(match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
+  case '<':
+    return make_token(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+  case '>':
+    return make_token(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+  }
+
+  if (is_at_end()) {
+    return make_token(TOKEN_EOF);
+  }
+
+  return error_token("Unexpected character.");
+}
